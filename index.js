d0c3b3733d378dc5262913c08b5a630951d9495b
potistudio02 <102900978+potistudio02@users.noreply.github.com>
2022-07-03 12:32:17 +0900

    Add Home Menu

--- index.js
+++ index.js
@@ -1,21 +1,43 @@
+// CONST Variales that Contain DOM Element
 const questionText = document.getElementById ("questionText");
 const answerText = document.getElementById ("answerText");
+const homeButton = document.getElementById ("homeButton");
+
 const questionsLength = questions.length;
 
 let questionIndex = 0;
 let isTranslatable = true;
+let currentTask;
 
-document.getElementById ("questionCard").addEventListener ("click", () => {
-	if (isTranslatable)
-		nextCard();
-	else
-		showAnswer();
-});
+class Task {
+	questions;
+
+	constructor (questions) {
+		this.questions = questions;
+	}
+
+	getQuestionByIndex (index) {
+		return questions[index];
+	}
+}
+
+!function() {
+	document.getElementById ("questionCard").addEventListener ("click", () => {
+		if (isTranslatable)
+			nextCard();
+		else
+			showAnswer();
+	});
+
+	for (let i = 0; i < taskList.length; i++)
+		createCard (taskList[i].name, taskList[i].title, taskList[i].description);
+}();
 
 function nextCard() {
 	hideAnswer();
-	questionIndex = randomInt(0, questionsLength - 1);
-	questionText.innerText = questions[questionIndex].q;
+	questionIndex = randomInt(0, currentTask.questions.length - 1);
+
+	questionText.innerText = currentTask.getQuestionByIndex(questionIndex).q;
 
 	anime ({
 		targets: questionText,
@@ -46,6 +68,65 @@ function hideAnswer() {
 	answerText.style.visibility = "hidden";
 }
 
+homeButton.addEventListener ("click", () => {
+	goHome();
+});
+
+function goHome() {
+	document.getElementById ("home").style.visibility = "visible";
+	document.getElementById ("main").style.visibility = "hidden";
+	hideAnswer();
+}
+
+function goTask() {
+	document.getElementById ("home").style.visibility = "hidden";
+	document.getElementById ("main").style.visibility = "visible";
+	questionIndex = 0;
+}
+
+function createCard (name, title, description, path) {
+	let element = document.createElement ("div");
+	let titleText = document.createElement ("span");
+	let descriptionText = document.createElement ("span");
+
+	element.classList.add ("Task-Card");
+	titleText.classList.add ("Task-Title");
+	descriptionText.classList.add ("Task-Description");
+
+	element.id = "taskCard";
+
+	titleText.innerText = title;
+	descriptionText.innerText = description;
+
+	element.appendChild (titleText);
+	element.appendChild (descriptionText);
+
+	element.addEventListener ("click", () => {
+		loadTask(name);
+		goTask();
+	});
+
+	document.getElementById ("home").appendChild (element);
+}
+
+function loadTask (taskName) {
+	let element = document.createElement ("script");
+	element.src = findArray (taskList, "name", taskName).path;
+	document.body.appendChild (element);
+
+	element.onload = () => {
+		currentTask = new Task (questions);
+	}
+}
+
 function randomInt (min, max) {
 	return Math.round (Math.random() * (max - min) + min);
+}
+
+function findArray (array, key, value) {
+	for (let i = 0; i < array.length; i++) {
+		if (array[i][key] == value) {
+			return array[i];
+		}
+	}
 }
