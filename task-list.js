6b2a79bf1f584b6e87203c5f724e55acfc625ad9
potistudio02 <102900978+potistudio02@users.noreply.github.com>
2022-07-03 15:55:57 +0900

    Update

--- task-list.js
+++ task-list.js
@@ -10,5 +10,17 @@ var taskList = [
 		title: "英単語",
 		description: "Unit 1 ~",
 		path: "./English-Words-Unit1.js"
+	},
+	{
+		name: "History",
+		title: "歴史総合",
+		description: "教科書 : p49, p52, p53, p56 ~ p59",
+		path: "./tasks/History-11B.js"
+	},
+	{
+		name: "PE",
+		title: "保健体育",
+		description: "学習プリント : No.1 ~ No.7",
+		path: "./tasks/PE-11B.js"
 	}
 ]
